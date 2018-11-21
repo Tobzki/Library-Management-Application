c23bb433b93ca1b92821ea87e82e34bd2d779c06
rasmusnilsson-dev <info@rasmusnilsson.se>
2018-11-21 11:30:22 +0100

    Added something to sout in main

--- Library Management Application/src/com/company/Main.java
+++ Library Management Application/src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello Rasmus and Håkan!");
+        System.out.println("Hello Rasmus and Håkan, don't forget Tobbe!");
     }
 }
