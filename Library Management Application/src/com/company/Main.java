5e016f1d057abee037ff2a77e61d73d03ede18af
rasmusnilsson-dev <info@rasmusnilsson.se>
2018-11-21 14:25:37 +0100

    Changed main.java for testing

--- Library Management Application/src/com/company/Main.java
+++ Library Management Application/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello Rasmus and Håkan, don't forget Tobbe!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Jag och mina böjs");
+        }
     }
 }
