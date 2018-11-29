d948dcefd6d3c0f96ed2d625d4d02e2d3104e060
rasmusnilsson-dev <info@rasmusnilsson.se>
2018-11-29 12:41:33 +0100

    Created helper method for int inputs.

--- Library Management Application/src/com/company/LibraryApp.java
+++ Library Management Application/src/com/company/LibraryApp.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class LibraryApp {
 
     public static void main(String[] args) {
 
+    }
+
+    public static int safeIntInput (String prompt) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(prompt + " >> ");
+        int result = -2;
+        do {
+            if (result == -1) {
+                System.out.print(prompt + " >> ");
+            }
+            try {
+                result = input.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Enter a valid input.");
+                input.nextLine();
+                result = -1;
+            }
+        } while(result == -1);
+
+        return result;
     }
 }
