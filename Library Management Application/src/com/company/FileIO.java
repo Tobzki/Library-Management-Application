e59cb172d82d56bbc19f173fbdeba82552ba6794
rasmusnilsson-dev <info@rasmusnilsson.se>
2018-12-10 13:39:41 +0100

    Added FileIO

---
+++ Library Management Application/src/com/company/FileIO.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+
+public class FileIO {
+
+    public static void out (String file, ArrayList<String> output) {
+        Path path = Paths.get(FileSystems.getDefault().getPath("Histories/" + file + ".txt").toString());
+        ArrayList<String> content = new ArrayList<>();
+        output.add("******************************");
+        try {
+            Files.write(path, output, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void create (String name) {
+        Path path = Paths.get(FileSystems.getDefault().getPath("Histories/" + name + ".txt").toString());
+        ArrayList<String> header = new ArrayList<>();
+        header.add("******************************");
+        header.add(String.format("History for user: %s", name));
+        header.add("******************************");
+        try {
+            Files.write(path, header, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
